1fa0df05e6e1d4f2de8e3eacdb7ff4c47e239131
Sergey Goliney <sergey@goliney.com>
2016-06-12 18:21:51 +0300

    Fixing indentations

--- tasks/coderoom.js
+++ tasks/coderoom.js
@@ -8,40 +8,40 @@
 
 'use strict';
 
-module.exports = function(grunt) {
-  var coderoom = require('coderoom');
+module.exports = function (grunt) {
+    var coderoom = require('coderoom');
 
-  grunt.registerMultiTask('coderoom', 'Grunt plugin for Coderoom', function() {
+    grunt.registerMultiTask('coderoom', 'Grunt plugin for Coderoom', function () {
 
-    if (this.files.length !== 1) {
-      grunt.fail.warn(`single source-destination pair should be provided. Got ${this.files.length}.`);
-    }
+        if (this.files.length !== 1) {
+            grunt.fail.warn(`single source-destination pair should be provided. Got ${this.files.length}.`);
+        }
 
-    var srcDest = this.files[0];
-    var src = srcDest.src[0];     // src always array
-    var dest = srcDest.dest;
+        var srcDest = this.files[0];
+        var src = srcDest.src[0];     // src always array
+        var dest = srcDest.dest;
 
-    if (srcDest.src.length !== 1) {
-      grunt.fail.warn(`single source path should be provided. Got ${srcDest.src.length}.`);
-    }
+        if (srcDest.src.length !== 1) {
+            grunt.fail.warn(`single source path should be provided. Got ${srcDest.src.length}.`);
+        }
 
-    if (!grunt.file.exists(src)) {
-      grunt.fail.warn(`source ${src} doesn't exist.`);
-    }
+        if (!grunt.file.exists(src)) {
+            grunt.fail.warn(`source ${src} doesn't exist.`);
+        }
 
-    if (!grunt.file.isDir(src)) {
-      grunt.fail.warn(`source ${src} is not a directory.`);
-    }
+        if (!grunt.file.isDir(src)) {
+            grunt.fail.warn(`source ${src} is not a directory.`);
+        }
 
-    var kindOfDest = grunt.util.kindOf(srcDest.dest);
-    if (kindOfDest !== 'string') {
-      grunt.fail.warn(`destination directory must be a string. Got ${srcDest.dest} type of ${kindOfDest} instead.`);
-    }
+        var kindOfDest = grunt.util.kindOf(srcDest.dest);
+        if (kindOfDest !== 'string') {
+            grunt.fail.warn(`destination directory must be a string. Got ${srcDest.dest} type of ${kindOfDest} instead.`);
+        }
 
-    var start = process.hrtime();
-    coderoom.build(src, dest);
-    var elapsed = (process.hrtime(start)[1] / 1000000000).toFixed(3); // divide by a million to get nano to seconds
+        var start = process.hrtime();
+        coderoom.build(src, dest);
+        var elapsed = (process.hrtime(start)[1] / 1000000000).toFixed(3); // divide by a million to get nano to seconds
 
-    grunt.log.ok(`Coderoom built into ${dest} (${elapsed}s)`);
-  });
+        grunt.log.ok(`Coderoom built into ${dest} (${elapsed}s)`);
+    });
 };
